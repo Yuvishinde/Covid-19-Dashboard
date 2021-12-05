7d4488a11e9b82dc09a1f24d076aeec08d07593c
Yuvraj <shindeyuvi123@gmail.com>
2021-12-05 20:00:45 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "../node_modules/bootstrap/dist/css/bootstrap.css"
+import './App.css';
+import Statewise from "./components/statewiseData/stateWise"
+import'./components/statewiseData/statewise.css'
+
+
+const App = () => {
+  return(
+    <>
+  
+    <Statewise />
+    </>
+  )
+}
+
+export default App
